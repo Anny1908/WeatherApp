f79e40c56d2ceb169217cf1cba743817bbff820d
Anny <annylianmarian@gmail.com>
2020-10-16 14:08:47 +0200

    tareas continuacion

--- app.js
+++ app.js
@@ -43,62 +43,76 @@ const showWeatherInfo = info => {
     const pais = document.getElementById("pais");
     pais.innerText = country;
 };
-const callWeather = (latitude, longitude) => {
-    const call = fetch(`${baseURL}lat=${latitude}&lon=${longitude}&appid=${APIkey}`);
-    call.then(response => response.json()).then(weatherInfo => showWeatherInfo(weatherInfo));
-    call.catch(error => console.error(error));
-};
-/*const searchResultsList = results => {
+// revisar esta funcion
+const cities = [];
+const searchResultsList = results => {
     console.log("resulst", results);
-    const resultsList = document.getElementById("results");​
-    const { list: citiesList } = results;​
-    citiesList.map(city => {
+    const resultsList = document.getElementById("results");
+    cities.push({
+        results
+    });
+    console.log("2", cities);
+    cities.map(city => {
         console.log("==>>>", city);
         const {
             main: { temp },
             name,
             sys: { country },
             weather: [{ description, icon }]
-        } = city;​
+        } = city.results;
         const listItem = document.createElement("li");
-        listItem.className = "list-item";
         listItem.innerHTML = `${name}, ${country}, ${description}, ${convertKelvinToCelsius(temp)}°C`;
         const img = document.createElement("img");
-        img.className = "list-item-icon";
         img.src = `icons/${icon}.png`;
         listItem.appendChild(img);
-        const addButton = document.createElement("button");
-        addButton.onclick = () => console.log("Soy el elemento ", listItem.innerHTML);
-        listItem.appendChild(addButton);​
         resultsList.appendChild(listItem);
+        console.log("lista", listItem);
+        console.log("UL", resultsList);
     });
-};
-*/
-const searchCityWeather = () => {
-    const ciudad = "london";
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=95d6b760ca648908e702eff5f26d03c8`)
-        .then(response => response.json())
-        .then((weatherSearchInfo) => { console.log(weatherSearchInfo) })
-        .catch((error) => console.log(error));
 
+
+};
+/*const cities = [];
+cities.push({
+    name: weatherSearchInfo.name,
+    latitude: weatherSearchInfo.coord.lat,
+    longitude: weatherSearchInfo.coord.lon
+});
+console.log("esto", cities);*/
+
+
+const callWeather = (latitude, longitude) => {
+    const call = fetch(`${baseURL}lat=${latitude}&lon=${longitude}&appid=${APIkey}`);
+    call.then(response => response.json()).then(weatherInfo => showWeatherInfo(weatherInfo));
+    call.catch(error => console.error(error));
 };
 
 function handleOnSearch() {
-    const text = document.getElementById("searchInput").value;
-    console.log("lo que vamos a buscar es ", text);​
+    const text = entrada.value;
     if (text) {
         searchCityWeather(text.toLowerCase());
     }
+    console.log(entrada.value)
+    return text;
 };
+handleOnSearch();
+
+const searchCityWeather = () => {
+    const ciudad = entrada.value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=95d6b760ca648908e702eff5f26d03c8`)
+        .then(response => response.json())
+        .then(weatherSearchInfo => {
+            console.log(weatherSearchInfo.name);
+            searchResultsList(weatherSearchInfo)
+        })
+        //{console.log(weatherSearchInfo.coord.lat) })
+        .catch((error) => console.log(error));
+};
+
 // recien incluido
 boton.addEventListener("click", () => {
     handleOnSearch();
 })
-
-entrada.addEventListener("click", () => {
-    handleOnSearch();
-})
-
 
 navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
